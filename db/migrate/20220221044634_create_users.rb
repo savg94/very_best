aeeb141958482eaf40b25c3beb2137eb3b7833a9
anonymous anonymous <savg94@gmail.com>
2022-02-21 04:46:35 +0000

    Generate users resource with ideas:scaffold

---
+++ db/migrate/20220221044634_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.integer :dish_id
+      t.integer :venue_id
+
+      t.timestamps
+    end
+  end
+end
